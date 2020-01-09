d9b6c8d28f0facf7827af1d83919c6556bb680a8
dut-a <d.dut@live.com>
2020-01-08 20:37:35 -0600

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
-// Your JavaScript code goes here!
+// handle on DOM elements
+const errorModal = document.querySelector("#modal");
+const errorMsg = document.querySelector("#modal-message");
 
+// Hide the error modal
+hideError();
 
+let heartStates = { "♡": FULL_HEART, "♥": EMPTY_HEART };
+let littleHearts = document.querySelectorAll(".like-glyph");
+
+function littleHeartLiking(e) {
+  let urls = { good: "http://localhost:3000/dogs", bad: "funnyUrl" };
+  let lovingHeart = e.target;
+  mimicServerCall(urls.bad)
+    .then(data => {
+       lovingHeart.innerText = heartStates[lovingHeart.innerText];
+       lovingHeart.classList.toggle("activated-heart");
+    })
+    .catch(error => {
+      errorModal.classList.remove("hidden");
+      errorMsg.innerHTML = error.message;
+      setTimeout(() => { hideError() }, 5000);
+    });
+}
+
+for (let littleHeart of littleHearts) {
+  littleHeart.addEventListener("click", littleHeartLiking);
+}
+
+function hideError() {
+  errorModal.classList.add("hidden");
+}
 
 
 //------------------------------------------------------------------------------
