bc9e4b49972ac355b03f630ab4730f2585609eae
github username <github email address>
2020-01-09 10:05:51 -0600

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorDiv = document.getElementById('modal')
 
 // Your JavaScript code goes here!
 
+const glyphHeart = document.getElementsByClassName('like-glyph')[0]
 
+errorDiv.classList.add('hidden');
+
+// document.addEventListener('DOMContentLoaded', function(){
+//   errorDiv.classList.add('hidden');
+// })
+
+glyphHeart.addEventListener("click", function() {
+  console.log('clicked!')
+  if (glyphHeart.innerText === EMPTY_HEART){
+    glyphHeart.innerText = FULL_HEART
+  } else if (glyphHeart.innerHTML === FULL_HEART) {
+    glyphHeart.innerHTML = EMPTY_HEART
+  }
+  // document.getElementById('modal').display = "block";
+
+})
 
 
 //------------------------------------------------------------------------------
@@ -23,3 +41,8 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+// function myFunction() {
+//   var element = document.getElementById("myDIV");
+//   element.classList.add("mystyle");
+// }
